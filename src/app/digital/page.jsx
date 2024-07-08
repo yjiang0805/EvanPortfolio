02a45a24bc8db64705e46a9c96c297ad5f8bb41f
Yihan Jiang <yihanjiang04@gmail.com>
2024-07-08 17:42:03 -0400

    added digitals and paintings page

---
+++ src/app/digital/page.jsx
@@ -0,0 +1,21 @@
+import "@/app/globals.css";
+
+import Image from "next/image";
+import Masonry from "@mui/lab/Masonry";
+
+const imageSources = [
+  "/assets/images/digitals/digital1.png",
+  "/assets/images/digitals/digital2.png",
+  "/assets/images/digitals/digital3.png",
+  "/assets/images/digitals/digital4.png",
+];
+
+export default function Digital() {
+  return (
+    <Masonry columns={3} spacing={1} sx={{ maxWidth: 1200, margin: "auto" }}>
+      {imageSources.map((src, index) => (
+        <Image key={index} src={src} alt="Digital" width={400} height={400} />
+      ))}
+    </Masonry>
+  );
+}
