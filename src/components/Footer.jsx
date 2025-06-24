4a3ef31c380da5cd96d5cd524e6ecd48e10aa961
Yihan Jiang <yihanjiang04@gmail.com>
2025-06-24 15:47:51 -0400

    added support for more links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,22 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center h-16 text-black">
-      <p className="md:text-lg font-spiegel">Follow me on Instagram:</p>
+      <p className="md:text-lg font-spiegel">Follow my socials:</p>
       <a
-        href="https://www.instagram.com/neph_enee/"
+        href="https://www.instagram.com/evanpatlee_art"
         target="_blank"
         rel="noreferrer"
         className="ml-2"
       >
         <Image src="./instagram.svg" alt="Instagram" width={24} height={24} />
+      </a>
+      <a
+        href="https://x.com/Evan_pat_lee"
+        target="_blank"
+        rel="noreferrer"
+        className="ml-2"
+      >
+        <Image src="./twitter.svg" alt="X" width={24} height={24} />
       </a>
     </footer>
   );
